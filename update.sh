7efaea1456454c729dcd0cf1dd1af6313904c1d0
Ian Walter <pub@ianwalter.dev>
2020-09-24 13:41:36 +0000

    Removing code-server logic from update

--- update.sh
+++ update.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
 settingsPath=$HOME/Library/Application\ Support/Code/User
-codeServerPath=$HOME/.local/share/code-server/User
 if [[ $(uname) == 'Linux' ]]; then
-  if [[ -d $codeServerPath ]]; then
-    settingsPath=$codeServerPath
-  else
-    settingsPath=~/.config/Code/User/settings.json
-  fi
+  settingsPath=~/.config/Code/User/settings.json
 fi
 
 # Copy settings files.
 cp "$settingsPath/settings.json" .
 cp "$settingsPath/keybindings.json" .
 
-codeBin='code'
-if [[ `which code-server` ]]; then
-  codeBin=code-server
-fi
-
 # Copy installed extensions to extensions file.
-$codeBin --list-extensions > extensions.txt
+code --list-extensions > extensions.txt
